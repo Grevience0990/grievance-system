eef747d2b0fcc5085c0250bfea8160f3de091449
Grevience0990 <ihcrlproject@gmail.com>
2026-05-02 16:07:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'grievance.sietdkl@gmail.com'     # 🔴 Replace with your email
-app.config['MAIL_PASSWORD'] = 'rmbg gkja skpu qgsb'       # 🔴 Replace with your app password
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 
 mail = Mail(app)
